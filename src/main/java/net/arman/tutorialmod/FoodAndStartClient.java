2a1beef5153dffbf1e3a9e8c565f4d13b54a9a1c
Armando Esponda <armanesponda@gmail.com>
2025-09-14 00:59:37 -0500

    Initial Commit

---
+++ src/main/java/net/arman/tutorialmod/FoodAndStartClient.java
@@ -0,0 +1,11 @@
+package net.arman.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoodAndStartClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+    
+}
---
+++ src/main/java/net/arman/tutorialmod/FoodAndStartDataGenerator.java
@@ -0,0 +1,11 @@
+package net.arman.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FoodAndStartDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
